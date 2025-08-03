54bbe5e7c47dce980476b1def6345465e9e24fe1
Hycienth-Paul025 <166744375+Hycienth-Paul025@users.noreply.github.com>
2025-08-03 17:02:05 +0100

    new  navbar function added and aslo correct some errors in the FAQ Section

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
+//// NAVBAR ACTIVE LINKS ACCORDING TO ITS SECTIONS
+/* 
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((currentSection) => {
+    const sectionHeight = currentSection.offsetHeight,
+      sectionTop = currentSection.offsetTop - 72,
+      sectionId = currentSection.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".navbar-list a[href*=" + sectionId + "]"
+      );
+      console.log(sectionClass)
+
+
+      if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        sectionClass.classList.add("active-btn")
+      }
+      else{
+        sectionClass.classList.remove("active-btn")
+      }
+
+
+  });
+}); */
+
 //// NAVBAR SHADOW
 
 const Navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
+  window.scrollY >= 64
+    ? Navbar.classList.add("shadow")
+    : Navbar.classList.remove("shadow");
 
-  window.scrollY >= 64  
-  ? Navbar.classList.add("sticky") 
- :Navbar.classList.remove("sticky");
-
-
-
-  console.log("scrolled")
-})
-
+});
 
 //// NAVIGATION (NAV-BAR) FUNCTIONS
 const navCont = document.querySelector(".navbar-container");
@@ -36,32 +59,23 @@ const navLinks = document.querySelectorAll(".navbar-link");
 
 navLinks.forEach((link) =>
   link.addEventListener("click", () => {
-
     //remove class from the current link
-navLinks.forEach(btn => btn.classList.remove("active-btn"))
-// add class to the clicked link
+    navLinks.forEach((btn) => btn.classList.remove("active-btn"));
+    // add class to the clicked link
     link.classList.add("active-btn");
 
     /// remove both navbar-container and blurry background
-     navCont.classList.remove("slide-in");
-  navOverlay.classList.remove("visible");
-
+    navCont.classList.remove("slide-in");
+    navOverlay.classList.remove("visible");
   })
 );
 
-
 //// OVERLAY FUNCTION
 
-navOverlay.addEventListener("click", ()=>{
-
-     navCont.classList.remove("slide-in");
-       navOverlay.classList.remove("visible");
-
-})
-
-
-
-
+navOverlay.addEventListener("click", () => {
+  navCont.classList.remove("slide-in");
+  navOverlay.classList.remove("visible");
+});
 
 //// ACCORDION
 
@@ -81,23 +95,15 @@ accordionToggle.forEach((item) => {
   item.addEventListener("click", accordionEffect);
 });
 
-
 ////// HOW TO CREATE CAROOUSEL FUNCTION WITH JS WITHOUT DUPLICATION OR REPEATITION OF INVESTOR IMAGE
 
-
-/// Note that you must have your css styling already when doing this 
-/// you can as well do the duplication in the html for testing purpose to be sure of what ypure styling  
-
+/// Note that you must have your css styling already when doing this
+/// you can as well do the duplication in the html for testing purpose to be sure of what ypure styling
 
 const duplicate = document.querySelector(".investor-images").cloneNode(true);
 // this means duplicate or repeat this class eLement
 
-// make sure the cloneNode() value is set to true for proper cloning, repetition or duplication 
+// make sure the cloneNode() value is set to true for proper cloning, repetition or duplication
 
-
-parent= document.querySelector(".investor-content").appendChild(duplicate);
+parent = document.querySelector(".investor-content").appendChild(duplicate);
 // this means show the duplicated class element
-
-
-
-
